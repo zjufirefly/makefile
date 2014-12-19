489482343996bedf881e0b83fd7e2fe8dd22343a
hzfanbin <hzfanbin@corp.netease.com>
2014-12-19 14:10:58 +0800

    编译动态库

---
+++ prj5/src2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "inc2.h"
+
+
+void src2_func1()
+{
+	printf("src2_func1\n");
+}
+
+void src2_func2()
+{
+	printf("src2_func2\n");
+}
---
+++ prj6/src3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "inc1.h"
+
+
+void src3_func1()
+{
+	printf("src3_func1\n");
+}
+
+void src3_func2()
+{
+	printf("src3_func2\n");
+}
